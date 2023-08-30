2ec5ba368aee9e9df6c31423cfa12e61184d74d3
Roman Vladimirov <trueromanus@gmail.com>
2023-08-30 20:45:00 +0300

    * Moved same properties in options to single base class

--- src/PostgreSQL.Migrations.Console/Options/ForceRevertOptions.cs
+++ src/PostgreSQL.Migrations.Console/Options/ForceRevertOptions.cs
@@ -3,19 +3,7 @@
 namespace PostgreSQL.Migrations.Console.Options {
 
     [Verb ( "force-revert", HelpText = "Revert only one migration specified in parameter." )]
-    public class ForceRevertOptions {
-
-        [Option ( 'f', "files", Required = true, HelpText = "List of files containing migrations." )]
-        public IEnumerable<string> Files { get; set; } = Enumerable.Empty<string> ();
-
-        [Option ( 'c', "connectionStrings", Required = true, HelpText = "List of connection strings to which migrations will be applied." )]
-        public IEnumerable<string> ConnectionStrings { get; set; } = Enumerable.Empty<string> ();
-
-        [Option ( 's', "strategy", Default = "MigrationResolverAttribute", HelpText = "Select strategy for read migrations." )]
-        public string Strategy { get; set; } = "";
-
-        [Option ( 'g', "group", HelpText = "If you specify some group or groups (separated by commas), migrations will be filtered by these groups." )]
-        public string Group { get; set; } = "";
+    public class ForceRevertOptions : DatabaseAdjustments {
 
         [Option ( 'm', "migration", Required = true, HelpText = "The parameter specifies the number of the migration which will be reverted (if it was applied before) and after it applied once again." )]
         public int Migration { get; set; } = 0;
--- src/PostgreSQL.Migrations.Console/Options/RevertOptions.cs
+++ src/PostgreSQL.Migrations.Console/Options/RevertOptions.cs
@@ -3,19 +3,7 @@
 namespace PostgreSQL.Migrations.Console.Options {
 
     [Verb ( "revert", HelpText = "Revert database to state before migration specified in parameter." )]
-    public class RevertOptions {
-
-        [Option ( 'f', "files", Required = true, HelpText = "List of files containing migrations." )]
-        public IEnumerable<string> Files { get; set; } = Enumerable.Empty<string> ();
-
-        [Option ( 'c', "connectionStrings", Required = true, HelpText = "List of connection strings to which migrations will be applied." )]
-        public IEnumerable<string> ConnectionStrings { get; set; } = Enumerable.Empty<string> ();
-
-        [Option ( 's', "strategy", Default = "MigrationResolverAttribute", HelpText = "Select strategy for read migrations." )]
-        public string Strategy { get; set; } = "";
-
-        [Option ( 'g', "group", HelpText = "If you specify some group or groups (separated by commas), migrations will be filtered by these groups." )]
-        public string Group { get; set; } = "";
+    public class RevertOptions : DatabaseAdjustments {
 
         [Option ( 'm', "migration", Required = true, HelpText = "The parameter specifies the number of the migration to which you want to roll back the changes." )]
         public int Migration { get; set; } = 0;
