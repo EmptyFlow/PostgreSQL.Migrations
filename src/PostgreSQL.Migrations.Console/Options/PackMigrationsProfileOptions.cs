05d89c5bb46b4ca6b0735b7f1fb01b9e19b44d08
Roman Vladimirov <trueromanus@gmail.com>
2024-01-18 21:39:29 +0300

    + Added pack-profile verb

---
+++ src/PostgreSQL.Migrations.Console/Options/PackMigrationsProfileOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Migrations.Console.Options {
+
+	[Verb ( "pack-profile", HelpText = "Read migration and create single file containing all migrations." )]
+	public record PackMigrationsProfileOptions {
+
+		[Option ( 'p', "profile", Required = true, HelpText = "Path to profile file." )]
+		public string Profile { get; set; } = "";
+
+	}
+
+}
